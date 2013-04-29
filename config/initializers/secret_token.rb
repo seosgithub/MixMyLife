d23cc6bf5674edfedcd5d43e5ec6e0aa94cadf98
So Townsend <so.townsend@gmail.com>
2013-04-29 12:38:02 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MixMyLife::Application.config.secret_token = '4023f364646af0c4b270d1850e7e3e027b662b1c4fd220936d1e6192127b3f55d3fda495f365c8bdd4886c23d684c62fb37f29673920c9d90d0ea5cf9d978034'
